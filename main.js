39798cc6e456df28b037da1a57711a417ac6f084
Laura Long <lauralonggone@gmail.com>
2023-10-27 17:11:28 -0600

    Add the HTML and CSS, begin functionality

---
+++ main.js
@@ -0,0 +1,13 @@
+var recieveMessageButton = document.querySelector('.receiveMessageButton')
+var affirmationOption = document.querySelector('#affirmation')
+var mantraOption = document.querySelector('#mantra')
+var recieveMessageButton = document.querySelector('.receiveMessageButton')
+var recieveMessageButton = document.querySelector('.receiveMessageButton')
+var recieveMessageButton = document.querySelector('.receiveMessageButton')
+var recieveMessageButton = document.querySelector('.receiveMessageButton')
+
+
+// create variables for everything we are going to be selecting w/ QuerySel
+// functionality...
+// recieve message button 'click' will initiate the disappearance of the meditator icon and show a 
+// random message .. either affirmation or mantra (separate arrays)
